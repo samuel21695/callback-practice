c8c33082eebdf85dea6de540b337a12b2bb57978
samuel21695 <samuel21695@gmail.com>
2023-10-04 23:32:03 +0900

    delete bracket

--- ArrayFilter.js
+++ ArrayFilter.js
@@ -1,7 +1,7 @@
 words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
-// arrow function (function 쓸 필요 X)
-newWords = words.filter((element)=>{
+// parameter 하나일 경우 괄호 필요 x
+newWords = words.filter(element=>{
   return element.length > 6;
 });
 console.log(newWords);
