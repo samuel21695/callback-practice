69b583fdd990a852f38b78f4bc9c8d20acad46e6
samuel21695 <samuel21695@gmail.com>
2023-10-04 23:42:23 +0900

    myfilter

--- ArrayFilter.js
+++ ArrayFilter.js
@@ -1,6 +1,17 @@
 words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
-// 코드가 한줄인 경우 중괄호와 return도 필요 없음
-newWords = words.filter(element=> element.length > 6);
 
+// newWords = words.filter(element=> element.length > 6);
+// newWords와 똑같이 작동하는 myfilter 생성
+function myfilter(origin, callback){
+  var result = [];
+  for(var i=0; i<origin.length; i++) {
+    var current = origin[i];
+    if (callback(current)) {
+      result.push(current);
+    } 
+  }
+  return result;
+}
+newWords = myfilter(words, element=> element.length > 6);
 console.log(newWords);
